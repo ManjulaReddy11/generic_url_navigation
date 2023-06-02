f1dadcc93d80091a011f0b5d119960fdad342282
ManjulaReddy11 <manjulareddy497@gmail.com>
2023-06-02 15:11:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def praveen(request):
+    return render(request,'praveen.html')
+
+def manju(request):
+    return render(request,'manju.html')
